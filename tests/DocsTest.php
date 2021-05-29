54dc1847fce50148e5ab99e2ec1e55d7eab83dcd
Alexey Kopytko <alexey@kopytko.com>
2021-05-29 16:37:55 +0900

    Add date updated test

---
+++ tests/DocsTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright 2018 Alexey Kopytko <alexey@kopytko.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\PIndxTools;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * @coversNothing
+ *
+ * @internal
+ */
+final class DocsTest extends TestCase
+{
+    public function testStatusContainsValidDate(): void
+    {
+        $yaml = Yaml::parseFile('docs/_data/status.yml');
+
+        $this->assertArrayHasKey('updated', $yaml);
+        $this->assertNotNull($yaml['updated']);
+        $this->assertGreaterThan(
+            \time() - 3e+7,
+            $yaml['updated'],
+            \sprintf('Invalid date: %s', \date('Y-m-d', $yaml['updated']))
+        );
+    }
+}
